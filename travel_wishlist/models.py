4b657c3deede639958e4b594d16799fec58b356b
Giovanni DeTomaso <66696055+gdetomaso@users.noreply.github.com>
2024-11-20 13:40:32 -0600

    Added changes for lab 11

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,8 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
 
 class Place(models.Model):
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    notes = models.TextField(null=True, blank=True)
+    date_visited = models.DateField(null=True, blank=True)
+    photo = models.ImageField(upload_to='user_images/', null=True, blank=True)
+
+    #create save method to override default save method
+    def save(self, *args, **kwargs):
+        old_place = Place.objects.filter(pk=self.pk).first()
+        if old_place and old_place.photo:
+            if old_place.photo != self.photo:
+                self.delete_photo(old_place.photo)
+        
+        super().save(*args, **kwargs)
+
+    #delete method for deleting photo
+    def delete_photo(self, photo):
+        if default_storage.exists(photo):
+            default_storage.delete(photo)
+
+    #delete method for deleting photo when place is deleted
+    def delete(self, *args, **kwargs):
+        if self.photo:
+            self.delete_photo(self.photo)
+        super().delete(*args, **kwargs)
 
     def __str__(self):
-        return f'{self.name} visited: {self.visited}'
+        photo_str = self.photo.url if self.photo else 'no photo'
+        notes_str = self.notes[100:] if self.notes else 'no notes'
+        return f'{self.name} visited: {self.visited} on {self.date_visited}. Notes: {notes_str}. with photo: {photo_str}'
+        
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
-from .forms import NewPlaceForm
+from .forms import NewPlaceForm, TripReviewForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.contrib import messages
 
+@login_required
 def place_list(request):
 
     """
@@ -11,16 +15,17 @@ def place_list(request):
     """
     if request.method == 'POST':
         form = NewPlaceForm (request.POST)
-        place = form.save()
+        place = form.save(commit=False)
+        place.user = request.user
         if form.is_valid():
             place.save()
             return redirect('place_list')
 
-    places = Place.objects.filter(visited=False).order_by("name")
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by("name")
     new_place_form = NewPlaceForm()
     return render(request, "travel_wishlist/wishlist.html" , {"places": places, "new_place_form": new_place_form}) 
 
-
+@login_required
 def places_visited(request):
     
     """
@@ -30,7 +35,7 @@ def places_visited(request):
     """
     visited = Place.objects.filter(visited=True)
     return render(request, "travel_wishlist/visited.html", {"visited": visited})
-
+@login_required
 def place_was_visited(request, place_pk):
     """
     View to mark a place as visited. If the request is a POST 
@@ -40,10 +45,51 @@ def place_was_visited(request, place_pk):
     if request.method == 'POST':
         #place = Place.objects.get(pk=place_pk)
         place = get_object_or_404(Place, pk=place_pk)
-        place.visited = True
-        place.save()
+        if place.user == request.user:
+            place.visited = True
+            place.save()
+        else:
+            return HttpResponseForbidden()
     
     return redirect("place_list")
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    #return render(request, "travel_wishlist/place_detail.html", {"place": place})
+
+    #checks if place belongs to user
+    if place.user != request.user:
+        return HttpResponseForbidden()
+    
+    #if POST requests validate form data then update
+    if request.method == "POST":
+        form = TripReviewForm(request.POST, request.FILES, instance=place)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Trip Information updated")
+        else:
+            messages.error(request, form.errors)
+        return redirect("place_details", place_pk=place_pk)
+    #if GET request show Place info and form
+    else:
+        if place.visited:
+            review_form = TripReviewForm(instance=place)
+            return render(request, "travel_wishlist/place_detail.html", {"place": place, "review_form": review_form})
+        else:
+            return render(request, "travel_wishlist/place_detail.html", {"place": place})
+        
+@login_required
+def delete_place(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    if place.user == request.user:
+        place.delete()
+        return redirect("place_list")
+    else:
+        return HttpResponseForbidden()
+    
+
+
 
 def about(request):
     author = "Giovanni"
